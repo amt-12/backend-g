e31044d044618c64868fdf1d43ad777a4f0d335c
amt-12 <2020010004188@gndu.ac.in>
2025-03-08 18:47:40 +0530

    Add category routes to API and update routing structure

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authRoutes = require("./Auth.route");
+const categoryRoutes = require("./Category.route");
 
 
 router.use("/auth", authRoutes);
+router.use("/category", categoryRoutes);
 
 
 router.get("/ping", (req, res) => {
